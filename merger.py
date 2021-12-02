7b91c07c61106148d986c13250162591f87ce4a9
Anthony Gavazzi <anthonygavaz@yahoo.com>
2021-12-01 20:44:08 -0500

    parallelism working but slow

--- merger.py
+++ merger.py
@@ -1,10 +1,62 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from PyPDF2.pdf import PageObject
+from concurrent.futures import ThreadPoolExecutor, wait
+from time import time
+from io import BytesIO
+
+
+def merge2(rdr_in, startidx, bufidx, buf, do_print):
+    rdr = PdfFileReader(BytesIO(rdr_in.stream.getbuffer()))
+
+    if do_print:
+        print('Merging {} of {}'.format(int(startidx / 2) + 1, int(rdr.numPages / 2)))
+
+    pg1 = rdr.getPage(startidx)
+    pg2 = rdr.getPage(startidx + 1)
+
+    newpg = PageObject.createBlankPage(None, pg1.mediaBox.getWidth(), pg1.mediaBox.getHeight() * 2)
+    newpg.mergeTranslatedPage(pg1, 0, pg1.mediaBox.getHeight())
+    newpg.mergePage(pg2)
+
+    buf[bufidx] = newpg
+
+
+def merge_pdf(rdr: PdfFileReader, do_print=True):
+    # guaranteed if input comes from swapper, but necessary still
+    assert(rdr.numPages % 2 == 0)
+
+    MAXTHREADS = 32
+    buf = [None] * MAXTHREADS
+    wrtr = PdfFileWriter()
+    num_full_bufs = int(rdr.numPages/(MAXTHREADS*2))
+
+    for i in range(num_full_bufs):
+        with ThreadPoolExecutor(max_workers=MAXTHREADS) as executor:
+            futures = set()
+            for j in range(MAXTHREADS):
+                futures.add(executor.submit(merge2, rdr, i * MAXTHREADS * 2 + j * 2, j, buf, do_print))
+            wait(futures)
+
+            for pg in buf:
+                wrtr.addPage(pg)
+
+    with ThreadPoolExecutor(max_workers=MAXTHREADS) as executor:
+        futures = set()
+        rest = int((rdr.numPages % (MAXTHREADS * 2)) / 2)
+        for j in range(rest):
+            futures.add(executor.submit(merge2, rdr, num_full_bufs * MAXTHREADS * 2 + j * 2, j, buf, do_print))
+        wait(futures)
+
+        for j in range(rest):
+            pg = buf[j]
+            wrtr.addPage(pg)
+
+    return wrtr
 
 
 # Given a reader for a given pdf, every 2 pages vertically
 # Assume input pdf has page count divisible by 2 because it comes from the swapper function
-def merge_pdf(rdr: PdfFileReader, do_print=True):
+def merge_pdf_old(rdr: PdfFileReader, do_print=True):
     wrtr = PdfFileWriter()
     for i in range(0, rdr.numPages, 2):
         if do_print:
@@ -17,6 +69,7 @@ def merge_pdf(rdr: PdfFileReader, do_print=True):
         newpg = PageObject.createBlankPage(None, pg1.mediaBox.getWidth(), pg1.mediaBox.getHeight() * 2)
         newpg.mergeTranslatedPage(pg1, 0, pg1.mediaBox.getHeight())
         newpg.mergePage(pg2)
+
         wrtr.addPage(newpg)
 
     return wrtr
